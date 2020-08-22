99902e650693527b5921a9f0f5a19d174923d361
Vraja-nayaka <messiah.turner@gmail.com>
2020-08-22 21:38:33 +1100

    add jest and enzyme with typescript

---
+++ src/App.test.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import App from './App';
+
+test('should pass tests with tsx', () => {
+  expect(23).toBe(23);
+});
--- src/features/home.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export function HomePage() {
-  // no need to use useFirebaseApp - useUser calls it under the hood
-
-
-  return <h1>Welcome Back!</h1>
-}
