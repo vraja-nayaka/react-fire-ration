5e9a29018967d67b898c8e51cac89fe0372cb3c0
Vadim Tokar <messiah.turner@gmail.com>
2021-01-14 21:37:58 +0300

    add: display level

--- src/features/components/ProfileCard/index.tsx
+++ src/features/components/ProfileCard/index.tsx
@@ -1,5 +1,5 @@
 import React, { useMemo } from 'react';
-import { Box, Typography, LinearProgress, Paper } from '@material-ui/core';
+import { Box, LinearProgress, Paper, ListItemText } from '@material-ui/core';
 import { withStyles, lighten } from '@material-ui/core/styles';
 import { theme } from '../../../theme';
 
@@ -7,43 +7,48 @@ import ExperienceAvatar from './ExperienceAvatar';
 import { getNextLevelExperience } from './helpers';
 
 interface ProfileCardProps {
-    name: string;
-    avatar: string;
-    experience: number;
-    children?: React.ReactNode;
+  name: string;
+  avatar: string;
+  experience: number;
+  children?: React.ReactNode;
 }
 
 const BorderLinearProgress = withStyles({
-    root: {
-        height: 10,
-        backgroundColor: lighten(theme.palette.primary.main, 0.5),
-    },
-    bar: {
-        borderRadius: 20,
-        backgroundColor: theme.palette.primary.main,
-    },
+  root: {
+    height: 10,
+    backgroundColor: lighten(theme.palette.primary.main, 0.5),
+  },
+  bar: {
+    borderRadius: 20,
+    backgroundColor: theme.palette.primary.main,
+  },
 })(LinearProgress);
 
 const ProfileCard = (props: ProfileCardProps) => {
-    const { name, experience, avatar, children } = props;
-    const { experienceProgress } = useMemo(() => getNextLevelExperience(experience), [experience]);
+  const { name, experience, avatar, children } = props;
+  const { experienceProgress, level } = useMemo(() => getNextLevelExperience(experience), [experience]);
 
-    return (
-        <Paper>
-            <Box display="flex" alignItems="center" justifyContent="space-between" padding={1} minWidth={200}>
-                <ExperienceAvatar experience={experience} avatar={avatar} />
-                <Box marginX={2}>
-                    <Typography variant="h5">{name}</Typography>
-                </Box>
-                {children}
-            </Box>
-            <BorderLinearProgress
-                variant="determinate"
-                color="secondary"
-                value={experienceProgress}
+  return (
+    <Paper>
+      <Box display="flex" alignItems="center" justifyContent="space-between" padding={1} minWidth={200}>
+        <Box display="flex" alignItems="center">
+          <ExperienceAvatar experience={experience} avatar={avatar} />
+          <Box marginX={2}>
+            <ListItemText
+              primary={name}
+              secondary={`Уровень: ${level}`}
             />
-        </Paper >
-    );
+          </Box>
+        </Box>
+        {children}
+      </Box>
+      <BorderLinearProgress
+        variant="determinate"
+        color="secondary"
+        value={experienceProgress}
+      />
+    </Paper >
+  );
 };
 
 export default ProfileCard;
--- src/features/friends/components/FriendCard/index.tsx
+++ src/features/friends/components/FriendCard/index.tsx
@@ -4,54 +4,51 @@ import ControlPointDuplicateIcon from '@material-ui/icons/ControlPointDuplicate'
 import IndeterminateCheckBoxIcon from '@material-ui/icons/IndeterminateCheckBox';
 
 interface FriendCardProps {
-    avatar: string;
-    name: string;
-    userId: string;
-    level: string;
-    subscribeUser?: (id: string) => void;
-    unsubscribeUser?: (id: string) => void;
-    onClick?: () => void;
+  avatar: string;
+  name: string;
+  userId: string;
+  level: string;
+  subscribeUser?: (id: string) => void;
+  unsubscribeUser?: (id: string) => void;
+  onClick?: () => void;
 }
 
 const useStyles = makeStyles({
-    pointer: {
-        cursor: (props: boolean) => props ? 'pointer' : 'default',
-    },
+  pointer: {
+    cursor: (props: boolean) => props ? 'pointer' : 'default',
+  },
 });
 
 const FriendCard = (props: FriendCardProps) => {
-    const { avatar, name, userId, subscribeUser, unsubscribeUser, onClick } = props;
-    const classes = useStyles(!!onClick);
-    return (
-        <Paper elevation={3} className={classes.pointer}>
-            <ListItem onClick={() => onClick ? onClick() : false}>
-                <ListItemAvatar>
-                    <Avatar src={avatar} />
-                </ListItemAvatar>
-                <ListItemText
-                    primary={name}
-                    secondary={`Id: ${userId.substr(-8, 8)}`}
-                />
-                {/* <ListItemText
-                primary={`level: ${level}`}
-            /> */}
-                <ListItemSecondaryAction>
-                    {
-                        subscribeUser &&
-                        <IconButton edge="end" aria-label="subscribe" onClick={() => subscribeUser(userId)}>
-                            <ControlPointDuplicateIcon />
-                        </IconButton>
-                    }
-                    {
-                        unsubscribeUser &&
-                        <IconButton edge="end" aria-label="unsubscribe" onClick={() => unsubscribeUser(userId)}>
-                            <IndeterminateCheckBoxIcon />
-                        </IconButton>
-                    }
-                </ListItemSecondaryAction>
-            </ListItem>
-        </Paper>
-    );
+  const { avatar, name, userId, level, subscribeUser, unsubscribeUser, onClick } = props;
+  const classes = useStyles(!!onClick);
+  return (
+    <Paper elevation={3} className={classes.pointer}>
+      <ListItem onClick={() => onClick ? onClick() : false}>
+        <ListItemAvatar>
+          <Avatar src={avatar} />
+        </ListItemAvatar>
+        <ListItemText
+          primary={name}
+          secondary={`Уровень: ${level}, Id: ${userId.substr(-4, 4)}`}
+        />
+        <ListItemSecondaryAction>
+          {
+            subscribeUser &&
+            <IconButton edge="end" aria-label="subscribe" onClick={() => subscribeUser(userId)}>
+              <ControlPointDuplicateIcon />
+            </IconButton>
+          }
+          {
+            unsubscribeUser &&
+            <IconButton edge="end" aria-label="unsubscribe" onClick={() => unsubscribeUser(userId)}>
+              <IndeterminateCheckBoxIcon />
+            </IconButton>
+          }
+        </ListItemSecondaryAction>
+      </ListItem>
+    </Paper>
+  );
 };
 
 export default FriendCard;
